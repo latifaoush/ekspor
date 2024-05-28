a2e39ef97300fe71a51715674aaf490cb6b715cb
latifaoush <Latifafowh09@gmail.com>
2024-05-28 17:31:07 +0700

    Menghapus file hapus.php, koneksi.php, simpan.php, tambah_produk.php, ubah.php, update.php.

--- resources/views/hapus.php
+++
@@ -1,7 +0,0 @@
-<?php
-// include database connection file
-include base_path('resources/views/koneksi.php');
-$id = $_GET['id'];
-$result = mysqli_query($koneksi, "DELETE FROM produk WHERE id='$id'");
-header("Location:marketing.blade.php");
-?>
